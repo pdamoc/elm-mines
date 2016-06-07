8ece096ab230fb358aecec37c035f343c1366465
Peter Damoc <pdamoc@gmail.com>
2016-06-07 20:21:45 +0300

    Begin porting to 0.17

--- Matrix.elm
+++ Matrix.elm
@@ -1,58 +1,84 @@
-module Matrix where 
+module Matrix exposing (..)
 
 import Array exposing (Array, fromList)
 
-type alias Matrix a = Array (Array a)
+
+type alias Matrix a =
+    Array (Array a)
+
 
 repeat : Int -> Int -> a -> Matrix a
 repeat w h base =
     fromList <| List.repeat h <| fromList <| List.repeat w base
 
+
 get : Int -> Int -> Matrix a -> Maybe a
 get i j m =
-    let 
-        row = Array.get i m
-    in 
+    let
+        row =
+            Array.get i m
+    in
         case row of
-            Nothing -> Nothing
-            Just a -> Array.get j a
+            Nothing ->
+                Nothing
+
+            Just a ->
+                Array.get j a
+
 
 set : Int -> Int -> v -> Matrix v -> Matrix v
-set i j v m = 
-    let 
-        row = Array.get i m
-        row' = case row of
-            Nothing -> Nothing
-            Just a -> Just (Array.set j v a)
-    in 
+set i j v m =
+    let
+        row =
+            Array.get i m
+
+        row' =
+            case row of
+                Nothing ->
+                    Nothing
+
+                Just a ->
+                    Just (Array.set j v a)
+    in
         case row' of
-            Nothing -> m
-            Just newRow -> Array.set i newRow m
+            Nothing ->
+                m
+
+            Just newRow ->
+                Array.set i newRow m
+
 
 map : (a -> b) -> Matrix a -> Matrix b
 map f m =
-    let 
-        (w,h) = size m 
-   
+    let
+        ( w, h ) =
+            size m
+
         row i =
             Array.map f <| Maybe.withDefault (fromList []) <| Array.get i m
-    in 
-        Array.map row <| fromList [0..(h-1)]
-        
-indexedMap : (Int -> Int -> a -> b)-> Matrix a ->Matrix b
+    in
+        Array.map row <| fromList [0..(h - 1)]
+
+
+indexedMap : (Int -> Int -> a -> b) -> Matrix a -> Matrix b
 indexedMap f m =
-    let 
-        (w,h) = size m 
-   
+    let
+        ( w, h ) =
+            size m
+
         row i =
             Array.indexedMap (f i) <| Maybe.withDefault (fromList []) <| Array.get i m
-    in 
-        Array.map row <| fromList [0..(h-1)]
+    in
+        Array.map row <| fromList [0..(h - 1)]
 
-size : Matrix a -> (Int, Int)
+
+size : Matrix a -> ( Int, Int )
 size m =
-    let 
-        w = Array.length <| Maybe.withDefault (fromList []) <| Array.get 0 m
-        h = Array.length m  
-    in 
-        (w, h)
+    let
+        w =
+            Array.length <| Maybe.withDefault (fromList []) <| Array.get 0 m
+
+        h =
+            Array.length m
+    in
+        ( w, h )
